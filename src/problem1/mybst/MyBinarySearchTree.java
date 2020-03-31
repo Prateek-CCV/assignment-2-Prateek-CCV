52344c624a1a88241c4656728ffbec00f1b616ad
PRATEEK <prateekraj807@gmail.com>
2020-03-31 16:46:46 +0530

    created

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -23,4 +23,17 @@ public class MyBinarySearchTree {
         printInOrder(node.getLeftNode());
         System.out.println(node.getData());
     }
+
+    public void printInorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        printInOrder(node.getLeftNode());
+        System.out.println("The  node" + node.getData());
+        printInOrder(node.getRightNode());
+    }
+
+    void printInorder() {
+        printInOrder(root);
+    }
 }
