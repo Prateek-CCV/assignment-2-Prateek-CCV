190f0eda0df9a8fb565265497af129705d28704d
PRATEEK <prateekraj807@gmail.com>
2020-03-31 16:40:45 +0530

    created root node and printing

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,5 +6,21 @@
  */
 package problem1.mybst;
 // to implement BinarySearchTree
+
+import problem1.node.TreeNode;
+
 public class MyBinarySearchTree {
+    public TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void printInOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        printInOrder(node.getLeftNode());
+        System.out.println(node.getData());
+    }
 }
