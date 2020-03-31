a126002255f218fc207d00743c2a847c4a94cf71
PRATEEK <prateekraj807@gmail.com>
2020-03-31 16:52:19 +0530

    created main class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,19 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 public class MyMain {
+    public static void main(String[] args) {
+
+
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+        myBinarySearchTree.root = new TreeNode(45);
+        myBinarySearchTree.root = new TreeNode(30);
+        myBinarySearchTree.root = new TreeNode(15);
+        System.out.println("Print in inorder");
+        myBinarySearchTree.printInorder(myBinarySearchTree.root);
+    }
+
 }
