83ff89f8542eca8df7172d98ad8d149f6dc86dfa
PRATEEK <prateekraj807@gmail.com>
2020-03-31 17:21:36 +0530

    created main class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,22 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+
+
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+        myPriorityQueue.enquue(30);
+        myPriorityQueue.enquue(10);
+        myPriorityQueue.enquue(2);
+        myPriorityQueue.sort(myPriorityQueue.getRear());
+        System.out.println("Queue" + myPriorityQueue.toString());
+
+
+    }
 }
